da17502293c424ef3654dbb4f31820253c9a5950
Runxi Yu <me@runxiyu.org>
2025-11-24 08:00:00 +0800

    pack: Do not madvise
    Apparently although it's not fully sequential, the general pattern is
    sort-of increasing in offsets as trees are being traversed.

--- pack_pack.go
+++ pack_pack.go
@@ -534,17 +534,6 @@ func openPackFile(absPath, rel string) (*packFile, error) {
 		return nil, err
 	}
 
-	err = syscall.Madvise(region, syscall.MADV_RANDOM)
-	if err != nil {
-		_ = syscall.Munmap(region)
-		return nil, err
-	}
-	err = syscall.Madvise(region, syscall.MADV_WILLNEED)
-	if err != nil {
-		_ = syscall.Munmap(region)
-		return nil, err
-	}
-
 	return &packFile{
 		relPath: rel,
 		size:    stat.Size(),
