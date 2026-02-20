17d2a1d85e2747146031be1739df0b9109d112d8
Runxi Yu <me@runxiyu.org>
2026-02-20 12:41:47 +0800

    Revert "commitgraph: Add write stub"
    This reverts commit d65577ac3b55f3a52d1a507f3204a03df270b1ec.

--- commitgraph_write.go
+++
@@ -1,6 +0,0 @@
-package furgit
-
-// tbh we implemented quite a lot of writing commit graphs in
-// the tests for reading commit graphs, sometime i'll just extract
-// them into here instead and make the tests for both use the proper
-// routines.
