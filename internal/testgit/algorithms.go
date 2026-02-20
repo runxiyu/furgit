74b7854daa573894c8e725717567915bf55b22a0
Runxi Yu <me@runxiyu.org>
2026-02-21 00:23:57 +0800

    testgit: Use objectid's SupportedAlgorithms

--- internal/testgit/algorithms.go
+++ internal/testgit/algorithms.go
@@ -6,18 +6,10 @@ import (
 	"codeberg.org/lindenii/furgit/objectid"
 )
 
-// SupportedAlgorithms returns all object ID algorithms supported by furgit.
-func SupportedAlgorithms() []objectid.Algorithm {
-	return []objectid.Algorithm{
-		objectid.AlgorithmSHA1,
-		objectid.AlgorithmSHA256,
-	}
-}
-
 // ForEachAlgorithm runs a subtest for every supported algorithm.
 func ForEachAlgorithm(t *testing.T, fn func(t *testing.T, algo objectid.Algorithm)) {
 	t.Helper()
-	for _, algo := range SupportedAlgorithms() {
+	for _, algo := range objectid.SupportedAlgorithms() {
 		t.Run(algo.String(), func(t *testing.T) {
 			fn(t, algo)
 		})
