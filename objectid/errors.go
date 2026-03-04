4905605124ab0ea390cdd65963d5a6a1a5258c45
Runxi Yu <me@runxiyu.org>
2026-03-04 09:23:23 +0800

    objectid: File splitting

---
+++ objectid/errors.go
@@ -0,0 +1,10 @@
+package objectid
+
+import "errors"
+
+var (
+	// ErrInvalidAlgorithm indicates an unsupported object ID algorithm.
+	ErrInvalidAlgorithm = errors.New("objectid: invalid algorithm")
+	// ErrInvalidObjectID indicates malformed object ID data.
+	ErrInvalidObjectID = errors.New("objectid: invalid object id")
+)
