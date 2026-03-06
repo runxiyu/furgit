5da80869bce6a40584de32d8c18e10411b93db63
Runxi Yu <me@runxiyu.org>
2026-03-06 11:17:28 +0800

    diff/lines: Split files

--- diff/lines/diff.go
+++ diff/lines/diff.go
@@ -229,22 +229,3 @@ func Diff(oldB, newB []byte) ([]Chunk, error) { //nolint:maintidx
 
 	return out, nil
 }
-
-// Chunk represents a contiguous region of lines categorized
-// as unchanged, deleted, or added.
-type Chunk struct {
-	Kind ChunkKind
-	Data []byte
-}
-
-// ChunkKind enumerates the type of diff chunk.
-type ChunkKind int
-
-const (
-	// ChunkKindUnchanged represents an unchanged diff chunk.
-	ChunkKindUnchanged ChunkKind = iota
-	// ChunkKindDeleted represents a deleted diff chunk.
-	ChunkKindDeleted
-	// ChunkKindAdded represents an added diff chunk.
-	ChunkKindAdded
-)
