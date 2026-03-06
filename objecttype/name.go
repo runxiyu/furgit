bf70c5552d9c00b59ed271d0c8237c1c944ed074
Runxi Yu <me@runxiyu.org>
2026-03-06 10:44:22 +0800

    objecttype: Split files

---
+++ objecttype/name.go
@@ -0,0 +1,42 @@
+package objecttype
+
+const (
+	typeNameBlob   = "blob"
+	typeNameTree   = "tree"
+	typeNameCommit = "commit"
+	typeNameTag    = "tag"
+)
+
+// ParseName parses a canonical Git object type name.
+func ParseName(name string) (Type, bool) {
+	switch name {
+	case typeNameBlob:
+		return TypeBlob, true
+	case typeNameTree:
+		return TypeTree, true
+	case typeNameCommit:
+		return TypeCommit, true
+	case typeNameTag:
+		return TypeTag, true
+	default:
+		return TypeInvalid, false
+	}
+}
+
+// Name returns the canonical Git object type name.
+func Name(ty Type) (string, bool) {
+	switch ty {
+	case TypeBlob:
+		return typeNameBlob, true
+	case TypeTree:
+		return typeNameTree, true
+	case TypeCommit:
+		return typeNameCommit, true
+	case TypeTag:
+		return typeNameTag, true
+	case TypeInvalid, TypeFuture, TypeOfsDelta, TypeRefDelta:
+		return "", false
+	default:
+		return "", false
+	}
+}
