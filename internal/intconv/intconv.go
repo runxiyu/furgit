308bef16d6a1c73b0b135823637e2f06d00fb5db
Runxi Yu <me@runxiyu.org>
2026-03-06 08:04:32 +0800

    internal/intconv: Add Uint32ToUint8

--- internal/intconv/intconv.go
+++ internal/intconv/intconv.go
@@ -77,3 +77,12 @@ func SignExtendByteToUint32(b byte) uint32 {
 
 	return 0xFFFFFF00 | uint32(b)
 }
+
+// Uint32ToUint8 converts v to uint8, returning an error if it overflows.
+func Uint32ToUint8(v uint32) (uint8, error) {
+	if v > math.MaxUint8 {
+		return 0, fmt.Errorf("intconv: uint32 %d overflows uint8", v)
+	}
+
+	return uint8(v), nil
+}
