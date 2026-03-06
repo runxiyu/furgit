596b28df282888bfea41c1c67fa1fcb3e7cbf782
Runxi Yu <me@runxiyu.org>
2026-03-06 15:24:28 +0800

    objectstore: Add notes on a Cursor

--- objectstore/objectstore.go
+++ objectstore/objectstore.go
@@ -40,3 +40,9 @@ type Store interface {
 	// Close releases resources associated with the backend.
 	Close() error
 }
+
+// type Cursor any
+//
+// Then make all read functions accept and provide a Cursor
+// nil must always be accepted and would exhibit the same behavior as right now
+// Non-nil behavior is implementation-defined: e.g., pack selection
