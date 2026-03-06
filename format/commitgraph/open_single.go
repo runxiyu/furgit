f892037a0e4279e0f8114c5ebfe654afc931bffc
Runxi Yu <me@runxiyu.org>
2026-03-06 11:23:44 +0800

    format/commitgraph: Split files

---
+++ format/commitgraph/open_single.go
@@ -0,0 +1,32 @@
+package commitgraph
+
+import (
+	"os"
+
+	"codeberg.org/lindenii/furgit/internal/intconv"
+	"codeberg.org/lindenii/furgit/objectid"
+)
+
+func openSingle(root *os.Root, algo objectid.Algorithm) (*Reader, error) {
+	graph, err := openLayer(root, "info/commit-graph", algo)
+	if err != nil {
+		return nil, err
+	}
+
+	graph.baseCount = 0
+	graph.globalFrom = 0
+
+	hashVersion, err := intconv.Uint32ToUint8(algo.PackHashID())
+	if err != nil {
+		return nil, err
+	}
+
+	out := &Reader{
+		algo:        algo,
+		hashVersion: hashVersion,
+		layers:      []layer{*graph},
+		total:       graph.numCommits,
+	}
+
+	return out, nil
+}
