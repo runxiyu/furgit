1a8842b33d6d83d1754267e087fd7b4f73ca9e91
Runxi Yu <runxiyu@umich.edu>
2026-03-19 15:13:44 +0000

    object/stored: Use generics

--- object/stored/stored.go
+++ object/stored/stored.go
@@ -1,2 +1,33 @@
 // Package stored wraps parsed objects with their storage object IDs.
+//
+// Stored values are typically instantiated with pointer object types such as
+// *object.Blob, *object.Tree, *object.Commit, or *object.Tag, because those
+// pointer types satisfy object.Object.
 package stored
+
+import (
+	"codeberg.org/lindenii/furgit/object"
+	"codeberg.org/lindenii/furgit/objectid"
+)
+
+// Stored represents a stored object,
+// i.e., an object along with its object ID.
+type Stored[T object.Object] struct {
+	id  objectid.ObjectID
+	obj T
+}
+
+// New creates one stored object wrapper.
+func New[T object.Object](id objectid.ObjectID, obj T) *Stored[T] {
+	return &Stored[T]{id: id, obj: obj}
+}
+
+// ID returns the object ID.
+func (stored *Stored[T]) ID() objectid.ObjectID {
+	return stored.id
+}
+
+// Object returns the wrapped object as itself.
+func (stored *Stored[T]) Object() T {
+	return stored.obj
+}
