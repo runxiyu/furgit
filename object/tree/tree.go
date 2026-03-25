39d2d45d84a3bbae8a2be101c54271d1f0feccd4
Runxi Yu <runxiyu@umich.edu>
2026-03-25 18:15:48 +0000

    tree: Document that tree entries should be sorted

--- object/tree/tree.go
+++ object/tree/tree.go
@@ -3,5 +3,8 @@ package tree
 
 // Tree represents a Git tree object.
 type Tree struct {
+	// Entries must be sorted by TreeEntryNameCompare.
+	// You are strongly advised to use the methods for manipulation
+	// rather than modifying the slice yourself.
 	Entries []TreeEntry
 }
