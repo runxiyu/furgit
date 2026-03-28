59e99a5f6c93097f2bdc47d70a8c2f902cc92f43
Runxi Yu <runxiyu@umich.edu>
2026-03-28 18:33:25 +0000

    network/receivepack/hooks: Fix whitespace

--- network/receivepack/hooks/reject_force_push.go
+++ network/receivepack/hooks/reject_force_push.go
@@ -22,6 +22,7 @@ func RejectForcePush() receivepack.Hook {
 		_ = ctx
 
 		objects := objectmix.New(req.QuarantinedObjects, req.ExistingObjects)
+
 		defer func() { _ = objects.Close() }()
 
 		queries := commitquery.New(objects, req.CommitGraph)
