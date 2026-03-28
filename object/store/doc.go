d4c4ccd763a65518dfcc7b486808b0b6460b79be
Runxi Yu <runxiyu@umich.edu>
2026-03-28 15:52:51 +0000

    object/store: Document writing interface lack

--- object/store/doc.go
+++ object/store/doc.go
@@ -3,12 +3,4 @@
 // Concrete implementations generally inherit the contract documented by the
 // interfaces they satisfy. Implementation docs focus on additional guarantees
 // and implementation-specific behavior.
-//
-// There is currently no writing-store interface because different
-// object store backends have very different models for writing.
-// For example, a loose object store can trivially write single loose
-// objects, but writing individual objects to a packfile store would
-// be extremely wasteful.
-//
-// At some time, we will have writing-store interfaces.
 package objectstore
