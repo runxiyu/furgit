271cfaca0aeb31b89dd95ae72044200482e08985
Runxi Yu <runxiyu@umich.edu>
2026-03-29 09:46:26 +0000

    object: Update docs

--- object/object.go
+++ object/object.go
@@ -1,4 +1,9 @@
-// Package object provides shared object interfaces.
+// Package object provides the shared [Object] interface and parsing functions
+// for Git object values.
+//
+// Concrete object forms such as [blob], [tree], [commit], and [tag] live in
+// subpackages. Use [codeberg.org/lindenii/furgit/object/stored] when object
+// values need to be paired with the object IDs they were loaded under.
 package object
 
 import objecttype "codeberg.org/lindenii/furgit/object/type"
