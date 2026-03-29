642b21b80bcd43ff14355ef0287d7f57457d145e
Runxi Yu <runxiyu@umich.edu>
2026-03-29 16:20:47 +0000

    object/signed/commit: Add signed commit helpers

---
+++ object/signed/commit/payload_append.go
@@ -0,0 +1,11 @@
+package signedcommit
+
+// AppendPayload appends the commit verification payload to dst, omitting all
+// embedded signature headers.
+func (commit *Commit) AppendPayload(dst []byte) []byte {
+	for _, part := range commit.payload {
+		dst = append(dst, commit.body[part.start:part.end]...)
+	}
+
+	return dst
+}
---
+++ object/signed/commit/signature_append.go
@@ -0,0 +1,17 @@
+package signedcommit
+
+import objectid "codeberg.org/lindenii/furgit/object/id"
+
+// AppendSignature appends the unfolded signature for algo to dst.
+func (commit *Commit) AppendSignature(dst []byte, algo objectid.Algorithm) ([]byte, bool) {
+	signature, ok := commit.signatures[algo]
+	if !ok {
+		return dst, false
+	}
+
+	for _, part := range signature {
+		dst = append(dst, commit.body[part.start:part.end]...)
+	}
+
+	return dst, true
+}
