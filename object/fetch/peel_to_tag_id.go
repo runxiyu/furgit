1999e34a96a4b000a53e5f7565e2a5b0d61a90f4
Runxi Yu <runxiyu@umich.edu>
2026-03-31 02:13:09 +0000

    object/fetch: Remove peel to tag functions
    They were meaningless and confusing

--- object/fetch/peel_to_tag_id.go
+++
@@ -1,8 +0,0 @@
-package fetch
-
-import objectid "codeberg.org/lindenii/furgit/object/id"
-
-// PeelToTagID returns id unchanged.
-func (r *Fetcher) PeelToTagID(id objectid.ObjectID) (objectid.ObjectID, error) {
-	return id, nil
-}
