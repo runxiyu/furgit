a4503cb551712f4c18642d81675326c0e2cf65f3
Runxi Yu <runxiyu@umich.edu>
2026-04-02 07:17:22 +0000

    common: Document only for external APIs

--- common/doc.go
+++ common/doc.go
@@ -1,2 +1,7 @@
 // Package common encapsulates various helper packages not directly related to Git.
+//
+// Only those with APIs intended to be used by external modules and consumers,
+// such as those providing IO-related interfaces, belong here.
+// Internal-only helper packages belong in [codeberg.org/lindenii/furgit/internal]
+// or other internal packages.
 package common
