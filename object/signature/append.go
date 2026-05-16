73ee8f17a57209b83f0836cacc24aa59e23a40a7
Runxi Yu <runxiyu@umich.edu>
2026-05-16 20:15:24 +0000

    object/signature: Fix

--- object/signature/append.go
+++ object/signature/append.go
@@ -1,19 +1,18 @@
 package signature
 
 import (
-	"fmt"
-	"strconv"
 	"slices"
+	"strconv"
 )
 
-// Bytes renders the signature in canonical Git format.
-func (signature Signature) AppendTo(dst []byte) ([]byte) {
-	slices.Grow(dst, len(signature.Name) + len(signature.Email) + 32)
+// AppendTo renders the signature in canonical Git format.
+func (signature Signature) AppendTo(dst []byte) []byte {
+	dst = slices.Grow(dst, len(signature.Name)+len(signature.Email)+32)
 	dst = append(dst, signature.Name...)
 	dst = append(dst, ' ', '<')
 	dst = append(dst, signature.Email...)
 	dst = append(dst, '>', ' ')
-	strconv.AppendInt(dst, signature.WhenUnix, 10)
+	dst = strconv.AppendInt(dst, signature.WhenUnix, 10)
 	dst = append(dst, ' ')
 
 	offset := signature.OffsetMinutes
@@ -26,7 +25,9 @@ func (signature Signature) AppendTo(dst []byte) ([]byte) {
 
 	hh := offset / 60
 	mm := offset % 60
-	dst = fmt.Appendf(dst, "%c%02d%02d", sign, hh, mm)
+	dst = append(dst, byte(sign))
+	dst = append(dst, byte('0'+hh/10), byte('0'+hh%10))
+	dst = append(dst, byte('0'+mm/10), byte('0'+mm%10))
 
 	return dst
 }
