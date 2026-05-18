cbb50fb66c4a7c628574be01151046ca201de948
Runxi Yu <runxiyu@umich.edu>
2026-05-18 07:00:08 +0000

    common: Rewrap documentation

--- common/doc.go
+++ common/doc.go
@@ -1,7 +1,11 @@
-// Package common encapsulates various helper packages not directly related to Git.
+// Package common encapsulates various helper packages
+// not directly related to Git.
 //
-// Only those with APIs intended to be used by external modules and consumers,
-// such as those providing IO-related interfaces, belong here.
-// Internal-only helper packages belong in [codeberg.org/lindenii/furgit/internal]
+// Only those with APIs intended to be used
+// by external modules and consumers,
+// such as those providing IO-related interfaces,
+// belong here.
+// Internal-only helper packages
+// belong in [codeberg.org/lindenii/furgit/internal]
 // or other internal packages.
 package common
