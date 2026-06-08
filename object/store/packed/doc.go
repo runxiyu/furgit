a64316152e59c3d402f9904521aca9fbdf684f98
Runxi Yu <me@runxiyu.org>
2026-06-08 13:29:24 +0000

    object/store/packed: Add stubs

---
+++ object/store/packed/doc.go
@@ -0,0 +1,4 @@
+// Package packed provides Git object reading from,
+// and pack writing to,
+// an objects/pack directory.
+package packed
---
+++ object/store/packed/writer.go
@@ -0,0 +1,10 @@
+package packed
+
+import (
+	"io"
+
+	"lindenii.org/go/furgit/object/store"
+)
+
+// WritePack ingests one pack stream into the packed store.
+func (packed *Packed) WritePack(src io.Reader, opts store.PackWriteOptions) error
