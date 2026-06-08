7c3eaa3ddfc19b5334634faa50b4570433402b19
Runxi Yu <me@runxiyu.org>
2026-06-08 07:07:09 +0000

    object/fetch: Port tag

---
+++ object/fetch/tag.go
@@ -0,0 +1,26 @@
+package fetch
+
+import (
+	giterrors "lindenii.org/go/furgit/errors"
+	oid "lindenii.org/go/furgit/object/id"
+	"lindenii.org/go/furgit/object/stored"
+	"lindenii.org/go/furgit/object/tag"
+	"lindenii.org/go/furgit/object/typ"
+)
+
+// ExactTag reads, parses, and wraps the tag at id.
+//
+// Labels: Life-Parent.
+func (r *Fetcher) ExactTag(id oid.ObjectID) (*stored.Stored[*tag.Tag], error) {
+	parsed, err := r.parseObject(id)
+	if err != nil {
+		return nil, err
+	}
+
+	tag, ok := parsed.(*tag.Tag)
+	if !ok {
+		return nil, &giterrors.ObjectTypeError{OID: id, Got: parsed.ObjectType(), Want: typ.TypeTag}
+	}
+
+	return stored.New(id, tag), nil
+}
