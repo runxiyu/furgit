404d7c088e46fd1bf655a7cfe0b0217eadb719be
Runxi Yu <me@runxiyu.org>
2026-06-08 07:15:15 +0000

    object/fetch: port object

---
+++ object/fetch/object.go
@@ -0,0 +1,41 @@
+package fetch
+
+import (
+	"fmt"
+
+	"lindenii.org/go/furgit/object"
+	oid "lindenii.org/go/furgit/object/id"
+	"lindenii.org/go/furgit/object/stored"
+)
+
+// ExactObject reads, parses, and wraps the object at id without constraining
+// its concrete object kind.
+//
+// Labels: Life-Parent.
+func (r *Fetcher) ExactObject(id oid.ObjectID) (*stored.Stored[object.Object], error) {
+	parsed, err := r.parseObject(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return stored.New(id, parsed), nil
+}
+
+func (r *Fetcher) parseObject(id oid.ObjectID) (object.Object, error) {
+	ty, content, err := r.store.ReadBytesContent(id)
+	if err != nil {
+		return nil, wrapObjectReadError(id, err)
+	}
+
+	parsed, err := object.ParseWithoutHeader(ty, content, id.ObjectFormat())
+	if err != nil {
+		tyName, ok := ty.Name()
+		if !ok {
+			tyName = fmt.Sprintf("type %d", ty)
+		}
+
+		return nil, fmt.Errorf("object/fetch: parse object %s (%s): %w", id, tyName, err)
+	}
+
+	return parsed, nil
+}
