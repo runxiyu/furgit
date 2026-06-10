42cdcaa9a0d6d7ed6c776520f646d110ebf641dc
Runxi Yu <me@runxiyu.org>
2026-06-10 18:28:02 +0000

    internal/format/packfile: Add simple header functions

---
+++ internal/format/packfile/header.go
@@ -0,0 +1,9 @@
+package packfile
+
+// Signature is the 4-byte "PACK" magic at the start of pack files.
+const Signature = 0x5041434b
+
+// SupportedVersion reports whether a pack version is supported.
+func SupportedVersion(version uint32) bool {
+	return version == 2
+}
