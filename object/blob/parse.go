4582b264616b9f77f3d1f4a147d586a77359abbb
Runxi Yu <me@runxiyu.org>
2026-06-13 15:54:01 +0000

    object/blob: Lifetimes

--- object/blob/parse.go
+++ object/blob/parse.go
@@ -2,7 +2,13 @@ package blob
 
 // Parse decodes a blob object body.
 //
-// Labels: Life-Independent.
+// The returned blob aliases body:
+// its Data shares the same backing array,
+// so the blob inherits body's lifetime
+// and must not be mutated unless body may be.
+// Use [Blob.Clone] for an independent copy.
+//
+// Labels: Life-Parent, Mut-No.
 func Parse(body []byte) (*Blob, error) {
-	return &Blob{Data: append([]byte(nil), body...)}, nil
+	return &Blob{Data: body}, nil
 }
